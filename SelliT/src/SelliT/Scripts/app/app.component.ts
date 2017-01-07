fcc47e0586181454db88a793282ce4d2fbfc7e4c
DawdSarnecki <sarnecki.dawid@wp.pl>
2017-01-07 11:08:14 +0100

    added NPM package manager, Gulp task runner

---
+++ SelliT/src/SelliT/Scripts/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import {Component} from "@angular/core";
+@Component({
+    selector: "invoicelist",
+    template: `<h1>InvoiceList</h1><div>Loading, give as a while...</div>`
+})
+export class AppComponent { }
